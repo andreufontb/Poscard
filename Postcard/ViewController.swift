bff2e29d377afa7acc77c311aeb5c6fbd9c09d68
Andreu Font Bardolet <andreu@macmini.Local>
2014-10-22 16:07:10 +0200

    segon enviament

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //Afegint un comentari
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         messageLabel.hidden = false
